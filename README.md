99ec665907d0d09c3c036ff944247ccfd92fc900
SadPanda <>
2021-07-15 10:52:51 -0400

    Read the contents of DOCX files using Cobalt Strike's Execute-Assembly

---
+++ README.md
@@ -0,0 +1,2 @@
+# SharpSword
+ Read the contents of DOCX files using Cobalt Strike's Execute-Assembly
---
+++ SharpSword/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+using System.Xml;
+
+namespace SharpSword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine(@"
+
+ _____ _                      _____                       _ 
+/  ___| |                    /  ___|                     | |
+\ `--.| |__   __ _ _ __ _ __ \ `--.__      _____  _ __ __| |
+ `--. \ '_ \ / _` | '__| '_ \ `--. \ \ /\ / / _ \| '__/ _` |
+/\__/ / | | | (_| | |  | |_) /\__/ /\ V  V / (_) | | | (_| |
+\____/|_| |_|\__,_|_|  | .__/\____/  \_/\_/ \___/|_|  \__,_|
+                       | |                                  
+                       |_|                                  
+" +
+"" +
+"Developed By: @sadpanda_sec \n\n" +
+"Description: Read Contents of Word Documents (Docx).\n\n" +
+"Usage: SharpSword.exe C:\\Some\\Path\\To\\Document.docx");
+                System.Environment.Exit(0);
+
+            }
+            else if (args.Length == 1)
+            {
+                if (File.Exists(args[0]) && Path.GetExtension(args[0]).ToLower() == ".docx")
+                {
+                    var docPath = Path.GetFullPath(args[0]);
+
+                    using (var archive = ZipFile.OpenRead(docPath))
+                    {
+                     
+                        var xmlFile = archive.GetEntry(@"word/document.xml");
+                        if (xmlFile == null)
+                            return;
+
+                        using (var stream = xmlFile.Open())
+                        {
+                            using (var reader = new StreamReader(stream))
+                            {
+                                XmlDocument xmldoc = new XmlDocument();
+                                xmldoc.Load(stream);
+                                string plaintext = xmldoc.DocumentElement.InnerText;
+                                DateTime date = DateTime.Now;
+                                Console.WriteLine("\n" + date + ": " + "Reading Document: " + docPath + "\n\n");
+                                Console.WriteLine(plaintext);
+                                System.Environment.Exit(0);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("File Does Not Exist Or File Extention is Not DOCX");
+                    System.Environment.Exit(0);
+
+                }
+            }
+            else
+            {
+                if (args.Length > 1)
+                {
+                    Console.WriteLine("Error...Provided more than one command line argument\n\n" +
+                        "Usage: SharpSword.exe C:\\Some\\Path\\To\\Document.docx");
+                    System.Environment.Exit(0);
+                }
+            }
+        }
+
+    }
+        
+}
+
+
